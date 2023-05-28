df88b588796f152507739d6410a675ab29a34fe8
mijimenez9 <mario.jimenez17@tectijuana.edu.mx>
2023-05-28 02:29:21 -0700

    Updates

---
+++ precache-manifest.c49e257abe5e6e5ceea6c764019384ea.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a340621c92ad094e2aa3214735df5755",
+    "url": "/proyectoFinal-api-react/index.html"
+  },
+  {
+    "revision": "8ac532a2776cf9002f7b",
+    "url": "/proyectoFinal-api-react/static/css/2.a7cecf69.chunk.css"
+  },
+  {
+    "revision": "32acdc9d8d00b7807817",
+    "url": "/proyectoFinal-api-react/static/css/main.cc0f7ff6.chunk.css"
+  },
+  {
+    "revision": "8ac532a2776cf9002f7b",
+    "url": "/proyectoFinal-api-react/static/js/2.314334c0.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/proyectoFinal-api-react/static/js/2.314334c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32acdc9d8d00b7807817",
+    "url": "/proyectoFinal-api-react/static/js/main.dbeb23ac.chunk.js"
+  },
+  {
+    "revision": "d004c6c6a0d9fef44a65",
+    "url": "/proyectoFinal-api-react/static/js/runtime-main.9d3c88d0.js"
+  },
+  {
+    "revision": "1e9cd186b02026e9e7543dc7a5e7fd43",
+    "url": "/proyectoFinal-api-react/static/media/background.1e9cd186.webp"
+  },
+  {
+    "revision": "93d3a123d60d21cd6a693ec7c912d1e0",
+    "url": "/proyectoFinal-api-react/static/media/logoHorizontal.93d3a123.svg"
+  },
+  {
+    "revision": "f5bdff6225b63923b571d039f42d4519",
+    "url": "/proyectoFinal-api-react/static/media/logoYellow.f5bdff62.png"
+  },
+  {
+    "revision": "ed5ab9e580a8da57c01a7af70a900614",
+    "url": "/proyectoFinal-api-react/static/media/nave.ed5ab9e5.png"
+  },
+  {
+    "revision": "de1364c74d35ece81e42df588fff6a20",
+    "url": "/proyectoFinal-api-react/static/media/planeta.de1364c7.png"
+  },
+  {
+    "revision": "04ef2bb671bff96058b7a37fe3ef23b8",
+    "url": "/proyectoFinal-api-react/static/media/stormtrooper.04ef2bb6.png"
+  }
+]);
